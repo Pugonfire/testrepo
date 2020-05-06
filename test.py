4f21ffda0e1170cfd3644a471afa823a766442b6
Pugonfire <maxsis100@gmail.com>
2020-05-06 16:40:02 +0800

    added pet clas

--- test.py
+++ test.py
@@ -1,7 +1,20 @@
 print('test')
 
+
 class pet(object):
 
-    def __init__(self, name, age):
+    def __init__(self, name, age, species):
         self.name = name
         self.age = age
+        self.species = species
+
+
+def generatePet():
+    name = input('Name: ')
+    age = input('Age: ')
+    species = input('Species: ')
+    obj = pet(name, age, species)
+    return obj
+
+
+print(generatePet().name)
