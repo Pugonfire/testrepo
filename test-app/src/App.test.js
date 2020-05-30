23faa11927a0aa7a229bb60ed377c454f35d74f4
Pugonfire <maxsis100@gmail.com>
2020-05-30 11:32:12 +0800

    trying out react app

---
+++ test-app/src/App.test.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { expect } from "chai";
+import { mount, shallow } from "enzyme";
+import { render } from "@testing-library/react";
+import App from "./App";
+
+test("renders learn react link", () => {
+  const { getByText } = render(<App />);
+  const linkElement = getByText(/learn react/i);
+  expect(linkElement).toBeInTheDocument();
+});
