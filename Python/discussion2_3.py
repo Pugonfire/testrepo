5a5b4da5a3da6b2a3179712b9d3f0210fca6782b
Pugonfire <maxsis100@gmail.com>
2020-09-11 14:55:04 +0800

    Reorg

--- Python/discussion2_3.py
+++
@@ -1,9 +0,0 @@
-n = 0
-# Define variables 1 and 2 with the first two values
-val1 = 0
-val2 = 1
-while n < 1000:
-    print(val2)
-    # multiple assignment
-    val1, val2 = val2, (val1+val2)
-    n += 1
